283344c584ea016c721c252cd101feb17999843d
hoseokjang <72066274+hoseokjang@users.noreply.github.com>
2021-04-19 16:46:53 +0900

    Add files via upload

---
+++ project.py
@@ -0,0 +1,17 @@
+import requests
+from bs4 import BeautifulSoup as bs
+url = "http://www.ssg.com/disp/category.ssg?ctgId=6000094634"
+recvd = requests.get(url)
+dom = bs(recvd.text,'lxml')
+# print(dom)
+# 정육 > 돼지고기
+list = dom.find(class_='thmb')
+a = list.find_all('img')
+print(a)
+src = a[0]['src']
+src = 'http:'+src
+print(src)
+with open('image\\project\\test.jpg','wb') as f:
+    s = requests.get(src)
+    print(s)
+    f.write(s.content)
